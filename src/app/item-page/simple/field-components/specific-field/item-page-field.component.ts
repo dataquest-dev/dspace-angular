0355166686372192e236b0fd1d85b35d5d33b18a
MilanMajchrák <milan.majchak@dataquest.sk>
2022-04-01 11:10:40 +0200

    Check when is type date in the item view

--- src/app/item-page/simple/field-components/specific-field/item-page-field.component.ts
+++ src/app/item-page/simple/field-components/specific-field/item-page-field.component.ts
@@ -9,29 +9,45 @@ import { Item } from '../../../../core/shared/item.model';
  */
 
 @Component({
-    templateUrl: './item-page-field.component.html'
+  templateUrl: './item-page-field.component.html'
 })
 export class ItemPageFieldComponent {
 
-    /**
-     * The item to display metadata for
-     */
-    @Input() item: Item;
+  private _item;
 
-    /**
-     * Fields (schema.element.qualifier) used to render their values.
-     */
-    fields: string[];
+  // use getter setter to define the property
+  get item(): Item {
+    if (this.fields[0] === 'dc.date.issued') {
+      console.log(' returning');
+      console.log(this._item?.allMetadata(this.fields));
+      console.log(this.fields);
+      console.log(this.label);
+    }
+    return this._item;
+  }
 
-    /**
-     * Label i18n key for the rendered metadata
-     */
-    label: string;
+  /**
+   * The item to display metadata for
+   */
+  @Input()
+  set item(val: Item) {
+    this._item = val;
+  }
 
-    /**
-     * Separator string between multiple values of the metadata fields defined
-     * @type {string}
-     */
-    separator = '<br/>';
+  /**
+   * Fields (schema.element.qualifier) used to render their values.
+   */
+  fields: string[];
+
+  /**
+   * Label i18n key for the rendered metadata
+   */
+  label: string;
+
+  /**
+   * Separator string between multiple values of the metadata fields defined
+   * @type {string}
+   */
+  separator = '<br/>';
 
 }
