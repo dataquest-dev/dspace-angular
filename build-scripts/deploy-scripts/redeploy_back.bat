867507cc61084c0f5d1da544f64215e65653ed71
MajoBerger <marian.berger@dataquest.sk>
2022-04-25 11:57:08 +0200

    added pulling image before deploy and redeploy; using tag ..7_x instead of ..7_x-test

--- build-scripts/deploy-scripts/redeploy_back.bat
+++ build-scripts/deploy-scripts/redeploy_back.bat
@@ -1,2 +1,3 @@
+docker-compose -f deploy.yml pull dspace
 docker-compose -p our_dspace -f deploy.yml up -d dspace
 pause
--- build-scripts/deploy-scripts/redeploy_front.bat
+++ build-scripts/deploy-scripts/redeploy_front.bat
@@ -1,2 +1,3 @@
+docker-compose -f deploy.yml pull dspace-angular
 docker-compose -p our_dspace -f deploy.yml up -d dspace-angular
 pause
