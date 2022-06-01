dcae73c8bde9caebaf6d8c46c52daf06e8fa0329
jm server2 <jm@mazoea.com>
2022-06-01 17:49:54 +0200

    [devOps]

--- build-scripts/deploy-scripts/stop.bat
+++ build-scripts/deploy-scripts/stop.bat
@@ -1,3 +1,3 @@
-docker-compose -p dq-d7 -f ../../docker/docker-compose.yml -f ../../docker/docker-compose-rest.yml down
+docker-compose --env-file .env -p dq-d7 -f ../../docker/docker-compose.yml -f ../../docker/docker-compose-rest.yml down
 
 pause
