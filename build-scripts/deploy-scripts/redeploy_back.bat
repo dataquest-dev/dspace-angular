115025e6d225b5197a8edbcaee73593def614d59
jm server2 <jm@mazoea.com>
2022-06-02 11:30:04 +0200

    [devOps] TO REVERT - disable host check

--- build-scripts/deploy-scripts/redeploy_back.bat
+++ build-scripts/deploy-scripts/redeploy_back.bat
@@ -1,4 +1,4 @@
-docker-compose -p dq-d7 -f ../../docker/docker-compose.yml -f ../../docker/docker-compose-rest.yml pull dspace
-docker-compose -p dq-d7 -f ../../docker/docker-compose.yml -f ../../docker/docker-compose-rest.yml up -d --force-recreate dspace
+docker-compose --env-file .env -p dq-d7 -f ../../docker/docker-compose.yml -f ../../docker/docker-compose-rest.yml pull dspace
+docker-compose --env-file .env -p dq-d7 -f ../../docker/docker-compose.yml -f ../../docker/docker-compose-rest.yml up -d --force-recreate dspace
 
 pause
--- build-scripts/deploy-scripts/redeploy_front.bat
+++ build-scripts/deploy-scripts/redeploy_front.bat
@@ -1,4 +1,4 @@
-docker-compose -p dq-d7 -f ../../docker/docker-compose.yml --env-file .env -f ../../docker/docker-compose-rest.yml pull dspace-angular
-docker-compose -p dq-d7 -f ../../docker/docker-compose.yml --env-file .env -f ../../docker/docker-compose-rest.yml up -d --force-recreate dspace-angular
+docker-compose --env-file .env -p dq-d7 -f ../../docker/docker-compose.yml -f ../../docker/docker-compose-rest.yml pull dspace-angular
+docker-compose --env-file .env -p dq-d7 -f ../../docker/docker-compose.yml -f ../../docker/docker-compose-rest.yml up -d --force-recreate dspace-angular
 
 pause
