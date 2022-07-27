1ab69111365e193233fe8f98dba47ea18ce8b334
MilanMajchrák <milan.majchak@dataquest.sk>
2022-07-27 16:39:51 +0200

    Created sample tests for the HandleTable component

--- src/app/handle-page/handle-table/handle-table.component.spec.ts
+++ src/app/handle-page/handle-table/handle-table.component.spec.ts
@@ -23,4 +23,36 @@ describe('HandleTableComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initialize handleRoute', () => {
+    //
+  });
+
+  it('should initialize paginationOptions', () => {
+    //
+  });
+
+  it('should onInit should initialize handle table', () => {
+    // call getAllHandles()
+  });
+
+  it('should update handles in pageChange', () => {
+    //
+  });
+
+  it('should not allow to have two or more selected handles', () => {
+    //
+  });
+
+  it('should redirect with selected handle', () => {
+    //
+  });
+
+  it('should delete selected handle', () => {
+    //
+  });
+
+  it('should refresh table after delete', () => {
+    //
+  });
 });
