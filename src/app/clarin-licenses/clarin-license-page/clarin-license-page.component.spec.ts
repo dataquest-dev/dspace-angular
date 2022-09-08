dcd5bf67dd7bbf6e221971c516cef34ea8184141
MilanMajchrák <milan.majchak@dataquest.sk>
2022-09-08 14:49:29 +0200

    created clarin-license-page.component

---
+++ src/app/clarin-licenses/clarin-license-page/clarin-license-page.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ClarinLicensePageComponent } from './clarin-license-page.component';
+
+describe('ClarinLicensePageComponent', () => {
+  let component: ClarinLicensePageComponent;
+  let fixture: ComponentFixture<ClarinLicensePageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ClarinLicensePageComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ClarinLicensePageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/clarin-licenses/clarin-license-page/clarin-license-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ds-clarin-license-page',
+  templateUrl: './clarin-license-page.component.html',
+  styleUrls: ['./clarin-license-page.component.scss']
+})
+export class ClarinLicensePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
