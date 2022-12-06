555ebbc4381600dc2a21f5add84fc0f27dd8fa68
MilanMajchrák <milan.majchak@dataquest.sk>
2022-12-06 09:14:44 +0100

    Fixed unit tests

--- src/test-dtq.ts
+++ src/test-dtq.ts
@@ -15,10 +15,10 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
+// const context = require.context('./', true, /\.spec\.ts$/);
 
 // Find just one test for testing.
-// const context = require.context('./', true, /define-license-form.component.spec\.ts$/);
+const context = require.context('./', true, /clarin-navbar-top.component.spec\.ts$/);
 
 // And load the modules.
 context.keys().map(context);
