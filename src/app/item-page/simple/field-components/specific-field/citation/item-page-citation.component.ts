970aafb508dbccc6aa79f57c99541249ced1e028
HuynhKhoa1601 <huynhnhatkhoa21@gmail.com>
2023-06-05 18:47:48 +0700

    make the url as the constant value

--- src/app/item-page/simple/field-components/specific-field/citation/item-page-citation.component.ts
+++ src/app/item-page/simple/field-components/specific-field/citation/item-page-citation.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-
+import {CITACE_PRO_BASE_URL, UNIVERSITY_USING_DSPACE} from '../../../../../core/shared/tul/constants';
 @Component({
   selector: 'ds-item-page-citation-field',
   templateUrl: './item-page-citation.component.html',
@@ -9,10 +9,10 @@ export class ItemPageCitationFieldComponent {
   @Input() handle: string;
 
   constructor(private sanitizer: DomSanitizer) {
-  }
+}
 
   get iframeSrc(): SafeResourceUrl {
-    const url = `https://www.citacepro.com/api/dspace/citace/oai:dspace.tul.cz:${this.handle}`;
+    const url = `${CITACE_PRO_BASE_URL}:${UNIVERSITY_USING_DSPACE}:${this.handle}`;
     return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 }
