b8ef5574732b0f621cfebdb89f151fb2c5e2beff
HuynhKhoa1601 <huynhnhatkhoa21@gmail.com>
2023-06-13 19:12:37 +0700

    retrieve the constants from the BE

--- src/app/core/shared/tul/constants.ts
+++
@@ -1,3 +0,0 @@
-//Third party API
-export const CITACE_PRO_BASE_URL = 'https://www.citacepro.com/api/dspace/citace/oai';
-export const UNIVERSITY_USING_DSPACE = 'dspace.tul.cz';
--- src/app/item-page/simple/field-components/specific-field/citation/item-page-citation.component.ts
+++ src/app/item-page/simple/field-components/specific-field/citation/item-page-citation.component.ts
@@ -1,20 +1,47 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-import {
-  CITACE_PRO_BASE_URL,
-  UNIVERSITY_USING_DSPACE,
-} from '../../../../../core/shared/tul/constants';
+import { ConfigurationDataService } from 'src/app/core/data/configuration-data.service';
 @Component({
   selector: 'ds-item-page-citation-field',
   templateUrl: './item-page-citation.component.html',
 })
-export class ItemPageCitationFieldComponent {
+export class ItemPageCitationFieldComponent implements OnInit {
   @Input() handle: string;
 
-  constructor(private sanitizer: DomSanitizer) {}
+  citaceProStatus = true;
+  private citaceProURL: SafeResourceUrl | null;
 
-  get iframeSrc(): SafeResourceUrl {
-    const url = `${CITACE_PRO_BASE_URL}:${UNIVERSITY_USING_DSPACE}:${this.handle}`;
+  constructor(
+    private sanitizer: DomSanitizer,
+    private configService: ConfigurationDataService
+  ) {}
+
+  ngOnInit() {
+    this.configService.findByPropertyName('citace.pro.url').subscribe((remoteData) => {
+        const citaceProBaseUrl = remoteData.payload.values[0];
+        const universityUsingDspace = remoteData.payload.values[1];
+        this.citaceProURL = this.makeCitaceProURL(
+          citaceProBaseUrl,
+          universityUsingDspace
+        );
+    });
+
+    this.configService.findByPropertyName('citace.pro.status').subscribe((remoteData) => {
+      const citaceProBaseUrl = remoteData.payload.values[0];
+      this.citaceProStatus = citaceProBaseUrl === 'true';
+  });
+
+  }
+
+  makeCitaceProURL(
+    citaceProBaseUrl: string,
+    universityUsingDspace: string
+  ): SafeResourceUrl | null {
+    const url = `${citaceProBaseUrl}:${universityUsingDspace}:15240/151406`;
     return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
+  get iframeSrc(): SafeResourceUrl | null {
+    return this.citaceProURL;
   }
 }
