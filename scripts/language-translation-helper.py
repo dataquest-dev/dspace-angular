c099ff646f563578aada32e576793c18365c09cf
milanmajchrak <milan.majchrak@dataquest.sk>
2023-07-25 16:25:54 +0200

    Updated new czech translations

--- scripts/language-translation-helper.py
+++ scripts/language-translation-helper.py
@@ -8,7 +8,7 @@ txt_file_lines = []
 json_file_lines = []
 missing = []
 
-# with open("vocabulary-cs.csv", encoding="utf8") as csvfile:
+# with open("preklady-preložené-komplet.csv", encoding="utf8") as csvfile:
 #     csvreader = csv.reader(csvfile, delimiter=";")
 #
 #     for name, en, cs in csvreader:
@@ -20,43 +20,59 @@ missing = []
 #         txt_file_lines.append(line1 + line2 + line3)
 # txt_file_lines.append('}')
 
-# with open("vocabulary-cs.csv", encoding="utf8") as csvfile:
-#     csvreader = csv.reader(csvfile, delimiter=";")
-#
-#     for name, en, cs in csvreader:
-#         if name == '﻿id':
-#             continue
-#         txt_file_lines.append(name)
+with open("preklady-preložené-komplet.csv", encoding="utf8") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=";")
 
+    for name, en, cs in csvreader:
+        if name == '﻿id':
+            continue
+        txt_file_lines.append(name)
 
 with open('cs.json5', encoding="utf8") as f:
     json_p = json5.load(f)
-    for name in json_p:
-        json_file_lines.append(name)
-
-with open('en.json5', encoding="utf8") as f:
-    json_p = json5.load(f)
+#     print(json_p)
     for name in json_p:
         txt_file_lines.append(name)
 
-print('cs7.5: ' + str(len(json_file_lines)))
-print('en7.5: ' + str(len(txt_file_lines)))
-
-for name in txt_file_lines:
-    if name not in json_file_lines:
-        missing.append(name)
-
-
-# create json with translation which must be added into czech translation\
-translate = ['{']
-with open("en.json5", encoding="utf8") as f:
+# json_file_values = []
+with open('en.json5', encoding="utf8") as f:
     json_p = json5.load(f)
     for name in json_p:
-        if name not in missing:
-            continue
-        translate.append("\"" + name + "\"" + " : " + "\"" + json_p[name] + "\"" + "\n")
+        json_file_lines.append(name)
+        # json_file_values.append(json_p[name])
 
-translate.append('}')
+# with open('cs.json5', encoding="utf8") as f:
+#   json_p = json5.load(f)
+#   for name in json_p:
+#     line1 = '\n'
+#     line2 = '  //'
+#     if name in json_file_lines:
+#       line2 += ' \"' + str(name).strip() + '\"' + ' : ' + "\"" + str(json_file_values[json_file_lines.index(name)]).strip() + "\"" + "\n"
+#     line3 = '  \"' +  str(name).strip() + '\"' + ' : ' + "\"" + str(json_p[name]).strip() + "\"" + ","
+#     txt_file_lines.append(line1 + line2 + line3)
+#
+# txt_file_lines.append('}')
+
+
+print('en7.5: ' + str(len(json_file_lines)))
+print('cs7.5: ' + str(len(txt_file_lines)))
+#
+for name in json_file_lines:
+    if name not in txt_file_lines:
+      print('Are not in the en: ' + name)
+        # missing.append(name)
+#
+
+# create json with translation which must be added into czech translation\
+# translate = ['{']
+# with open("en.json5", encoding="utf8") as f:
+#     json_p = json5.load(f)
+#     for name in json_p:
+#         if name not in missing:
+#             continue
+#         translate.append("\"" + name + "\"" + " : " + "\"" + json_p[name] + "\"" + "\n")
+#
+# translate.append('}')
 #
 # with open('translated_cs_7.2.txt', 'w', encoding='utf-8') as f:
 #     f.write('\n'.join(txt_file_lines))
@@ -67,7 +83,7 @@ translate.append('}')
 # with open('en_7.2.txt', 'w', encoding='utf-8') as f:
 #     f.write('\n'.join(json_file_lines))
 
-# with open('translated_cs_7.2_json.txt', 'w', encoding='utf-8') as f:
+# with open('translated_cs_7.5_json.txt', 'w', encoding='utf-8') as f:
 #     f.write('\n'.join(txt_file_lines))
 #
 # with open('cs_7.5._to_translate_json.txt', 'w', encoding='utf-8') as f:
