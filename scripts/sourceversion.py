e101789b9a02927d6582ab8ee408b1b1802a419f
MajoBerger <marian.berger@dataquest.sk>
2024-03-01 08:14:06 +0100

    remove commented out code

--- scripts/sourceversion.py
+++ scripts/sourceversion.py
@@ -9,12 +9,7 @@ if __name__ == '__main__':
     print(f"<h4>This info was generated on: <br> <strong> {ts} </strong> </h4>")
 
     cmd = 'git log -1 --pretty=format:"<h4>Git hash: <br><strong> %H </strong> <br> Date of commit: <br> <strong> %ai </strong></h4>"'
-    # print(f">{cmd}")
     subprocess.check_call(cmd, shell=True)
-
-    # cmd = 'git status --porcelain'
-    # print(f">{cmd}:")
-    # subprocess.check_call(cmd, shell=True)
 
     link = sys.argv[1] + sys.argv[2]
     print('<br> <h4>Build run: </h4> <a href="' + link + '"> ' + link + '</a> ')
