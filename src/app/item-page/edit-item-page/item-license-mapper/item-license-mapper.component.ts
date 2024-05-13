cb844d6b77ddde1b9160320785e560780bd9afd5
milanmajchrak <milan.majchrak@dataquest.sk>
2024-05-13 13:54:37 +0200

    Added remove functionality.

--- src/app/item-page/edit-item-page/item-license-mapper/item-license-mapper.component.ts
+++ src/app/item-page/edit-item-page/item-license-mapper/item-license-mapper.component.ts
@@ -18,6 +18,7 @@ import { getItemPageRoute } from '../../item-page-routing-paths';
 import { NotificationsService } from '../../../shared/notifications/notifications.service';
 import { TranslateService } from '@ngx-translate/core';
 import { getLicensesManageTablePath, getLicensesModulePath } from '../../../app-routing-paths';
+import { isNull } from '../../../shared/empty.util';
 
 @Component({
   selector: 'ds-item-license-mapper',
@@ -45,6 +46,11 @@ export class ItemLicenseMapperComponent implements OnInit {
    */
   selectedLicenseId: number = null;
 
+  /**
+   * The current item UUID.
+   */
+  currentItemUUID: string;
+
   constructor(private route: ActivatedRoute,
               private router: Router,
               private clarinLicenseService: ClarinLicenseDataService,
@@ -64,6 +70,12 @@ export class ItemLicenseMapperComponent implements OnInit {
         take(1),
         map((data) => data.dso),
       );
+
+      this.itemRD$.pipe(
+        take(1))
+        .subscribe((itemRD) => {
+        this.currentItemUUID = itemRD.payload.uuid;
+      });
 
       this.loadCurrentLicense();
       this.loadAllLicenses();
@@ -119,6 +131,7 @@ export class ItemLicenseMapperComponent implements OnInit {
     }
 
   removeLicense() {
+    this.selectedLicenseId = null;
     this.currentLicense.next(null);
   }
 
@@ -126,32 +139,13 @@ export class ItemLicenseMapperComponent implements OnInit {
    * Update item's metadata and bitstream with new license and show notification on success/failure.
    */
   updateLicense() {
-    // Update item metadata with new license
-    this.itemRD$.pipe(
-      take(1),
-    ).subscribe((itemRD) => {
-      // Compose URL: /core/items/{itemUUID}/bundles?licenseID={licenseID}
-      let url = this.halService.getRootHref() + '/core/items/' + itemRD.payload.uuid + '/bundles?licenseID=' + this.selectedLicenseId;
-      const requestId = this.requestService.generateRequestId();
-      const putRequest = new PutRequest(requestId, url);
-      // call put request
-      this.requestService.send(putRequest);
-
-      // Process response
-      const response = this.rdbService.buildFromRequestUUID(requestId);
-      response
-        .pipe(getFirstCompletedRemoteData())
-        .subscribe((responseRD$: RemoteData<Item>) => {
-          if (hasFailed(responseRD$.state)) {
-            this.notificationsService.error(null, this.translateService.instant('item.edit.license.update.error'));
-            return;
-          }
-          // Update current license after successfully request
-          this.currentLicense.next(this.allLicenses.value.find(
-            (license) => String(license.id) === String(this.selectedLicenseId)));
-          this.notificationsService.success(null, this.translateService.instant('item.edit.license.update.success'));
-        });
-    });
+    if (isNull(this.currentLicense.value) && isNull(this.selectedLicenseId)) {
+      // Update bitstream license with none license - just detach license. `-1` is a special value for no license.
+      this.sendUpdateRequest(-1);
+    } else {
+      // Update bitstream license with new license - detach old license and attach a new license.
+      this.sendUpdateRequest(this.selectedLicenseId);
+    }
   }
 
   /**
@@ -162,5 +156,34 @@ export class ItemLicenseMapperComponent implements OnInit {
       const itemUrl = getItemPageRoute(itemRD.payload);
       void this.router.navigateByUrl(itemUrl);
     });
+  }
+
+  /**
+   * Send PUT request to update item's license. Show notification on success/failure.
+   * @param licenseId
+   * @private
+   */
+  private sendUpdateRequest(licenseId) {
+    // Compose URL: /core/items/{itemUUID}/bundles?licenseID={licenseID}
+    let url = this.halService.getRootHref() + '/core/items/' + this.currentItemUUID + '/bundles?licenseID=' + licenseId;
+    const requestId = this.requestService.generateRequestId();
+    const putRequest = new PutRequest(requestId, url);
+    // call put request
+    this.requestService.send(putRequest);
+
+    // Process response
+    const response = this.rdbService.buildFromRequestUUID(requestId);
+    response
+      .pipe(getFirstCompletedRemoteData())
+      .subscribe((responseRD$: RemoteData<Item>) => {
+        if (hasFailed(responseRD$.state)) {
+          this.notificationsService.error(null, this.translateService.instant('item.edit.license.update.error'));
+          return;
+        }
+        // Update current license after successfully request
+        this.currentLicense.next(this.allLicenses.value.find(
+          (license) => String(license.id) === String(licenseId)));
+        this.notificationsService.success(null, this.translateService.instant('item.edit.license.update.success'));
+      });
   }
 }
