286a1d489b034be9001a8796b27d6d278d4a6df5
milanmajchrak <90026355+milanmajchrak@users.noreply.github.com>
2024-05-27 08:23:19 +0200

    Cherry picked fixing of the workflow and resolved conflicts in the `lazy-theme.module.ts` (#625)
    * Cherry picked fixing of the workflow and resolved conflicts in the `lazy-theme.module.ts`
    
    * Fixed cycle imports
    
    * checkouted fixed test file from the vanilla
    
    ---------
    
    Co-authored-by: Tim Donohue <tim.donohue@lyrasis.org>

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -43,9 +43,6 @@ afterEach(() => {
 // https://github.com/DSpace-Labs/AIP-Files/releases/tag/demo-entities-data
 // (This is the data set used in our CI environment)
 
-// NOTE: FALLBACK_TEST_REST_BASE_URL is only used if Cypress cannot read the REST API BaseURL
-// from the Angular UI's config.json. See 'getBaseRESTUrl()' in commands.ts
-export const FALLBACK_TEST_REST_BASE_URL = 'http://localhost:8080/server';
 
 // Admin account used for administrative tests
 export const TEST_ADMIN_USER = Cypress.env('DSPACE_TEST_ADMIN_USER') || 'dspacedemo+admin@gmail.com';
@@ -73,5 +70,4 @@ export const TEST_WITHDRAWN_REASON = Cypress.env('CLARIN_TEST_WITHDRAWN_REASON')
 export const TEST_WITHDRAWN_REPLACEMENT = Cypress.env('CLARIN_TEST_WITHDRAWN_REPLACEMENT') || 'new URL';
 export const TEST_WITHDRAWN_AUTHORS = Cypress.env('CLARIN_TEST_WITHDRAWN_AUTHORS') || 'author1, author2';
 
-export const TEST_COLLECTION_NAME = 'Col';
 export const TEST_COMMUNITY_NAME = 'Com';
