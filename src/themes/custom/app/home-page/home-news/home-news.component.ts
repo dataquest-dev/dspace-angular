a7e692c3fcc6c6ad4c8325fad46fb9454d617e9e
milanmajchrak <milan.majchrak@dataquest.sk>
2024-07-31 10:28:51 +0200

    Show translated homepage image following the current locale.

--- src/themes/custom/app/home-page/home-news/home-news.component.ts
+++ src/themes/custom/app/home-page/home-news/home-news.component.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
 import { HomeNewsComponent as BaseComponent } from '../../../../../app/home-page/home-news/home-news.component';
+import { LocaleService } from '../../../../../app/core/locale/locale.service';
+
+const HOME_PAGE_IMAGE_PATH = 'assets/images/home-page-image-';
 
 @Component({
   selector: 'ds-home-news',
   styleUrls: ['./home-news.component.scss'],
   templateUrl: './home-news.component.html'
 })
-
 /**
  * Component to render the news section on the home page
  */
-export class HomeNewsComponent extends BaseComponent {}
+export class HomeNewsComponent extends BaseComponent {
 
+
+  constructor(private localeService: LocaleService) {
+    super();
+  }
+
+  /**
+   * Check if current czech or not
+   */
+  isCsLocale() {
+    return this.localeService.getCurrentLanguageCode() === 'cs';
+  }
+
+  /**
+   * Decide which image to show on the home page based on the current locale (cs, en)
+   */
+  getCorrectHomePageImage() {
+    return this.isCsLocale() ? HOME_PAGE_IMAGE_PATH + 'cs.png' : HOME_PAGE_IMAGE_PATH + 'en.png';
+  }
+}
