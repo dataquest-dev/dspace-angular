c1fb4a89de3f28e2c84eb9907a0f053f76a01501
milanmajchrak <milan.majchrak@dataquest.sk>
2024-08-09 15:29:01 +0200

    Fixed responsive issues

--- src/themes/dspace/app/header/header.component.ts
+++ src/themes/dspace/app/header/header.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent as BaseComponent } from '../../../../app/header/header.component';
+import { HOME_URL } from '../navbar/navbar.component';
+import { BehaviorSubject } from 'rxjs';
+import { MenuService } from '../../../../app/shared/menu/menu.service';
+import { HostWindowService } from '../../../../app/shared/host-window.service';
+import { Router } from '@angular/router';
 
 /**
  * Represents the header with the logo and simple navigation
@@ -10,4 +15,26 @@ import { HeaderComponent as BaseComponent } from '../../../../app/header/header.
   templateUrl: 'header.component.html',
 })
 export class HeaderComponent extends BaseComponent {
+  constructor(protected menuService: MenuService,
+              protected windowService: HostWindowService,
+              protected router: Router) {
+    super(menuService, windowService);
+  }
+
+  isHomePage: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+
+  ngOnInit(): void {
+    super.ngOnInit();
+
+    // Redirect to home page if the user is already on the home page to activate router event to hide the zcu icon
+    if (this.router.url === HOME_URL) {
+      void this.router.navigate([HOME_URL]);
+    }
+
+    this.router.events.subscribe(() => {
+      // Check if the current page is the home page. The `this.router.url === HOME_URL` check must be in this
+      // subscription.
+      this.isHomePage.next(this.router.url === HOME_URL);
+    });
+  }
 }
