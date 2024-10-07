98bbb5bc9b4434205d8cbb0e2b6a2e8cb61d20c0
milanmajchrak <milan.majchrak@dataquest.sk>
2024-10-07 14:35:19 +0200

    Updated tests following the new feature

--- src/app/share-submission/share-submission-page/share-submission-page.component.spec.ts
+++ src/app/share-submission/share-submission-page/share-submission-page.component.spec.ts
@@ -1,14 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ShareSubmissionPageComponent } from './share-submission-page.component';
+import { ActivatedRoute } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 describe('ShareSubmissionPageComponent', () => {
   let component: ShareSubmissionPageComponent;
   let fixture: ComponentFixture<ShareSubmissionPageComponent>;
+  let activatedRoute;
 
   beforeEach(async () => {
+    activatedRoute = {
+      snapshot: {
+        queryParams: new Map([
+          ['shareToken', 'fake-share-token'],
+        ])
+      }
+    };
+
     await TestBed.configureTestingModule({
-      declarations: [ ShareSubmissionPageComponent ]
+      declarations: [ ShareSubmissionPageComponent ],
+      imports: [
+        TranslateModule.forRoot()
+      ],
+      providers: [
+        { provide: ActivatedRoute, useValue: activatedRoute }
+      ]
     })
     .compileComponents();
 
