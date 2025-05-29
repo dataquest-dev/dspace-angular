fe9d312aec31b172125c7ae559d0be5d8ff703ad
Juraj Roka <95219754+jr-rk@users.noreply.github.com>
2025-05-29 16:29:45 +0200

    Edit of 403 forbidden page to show support mail and rephrased

--- src/app/forbidden/forbidden.component.ts
+++ src/app/forbidden/forbidden.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../core/auth/auth.service';
 import { ServerResponseService } from '../core/services/server-response.service';
+import { BehaviorSubject } from 'rxjs';
+import { ConfigurationDataService } from '../core/data/configuration-data.service';
 
 /**
  * This component representing the `Forbidden` DSpace page.
@@ -12,13 +14,15 @@ import { ServerResponseService } from '../core/services/server-response.service'
 })
 export class ForbiddenComponent implements OnInit {
 
+  emailToContact: BehaviorSubject<string> = new BehaviorSubject<string>('');
+
   /**
    * Initialize instance variables
    *
    * @param {AuthService} authService
    * @param {ServerResponseService} responseService
    */
-  constructor(private authService: AuthService, private responseService: ServerResponseService) {
+  constructor(private authService: AuthService, private responseService: ServerResponseService, private configService: ConfigurationDataService) {
     this.responseService.setForbidden();
   }
 
@@ -27,6 +31,9 @@ export class ForbiddenComponent implements OnInit {
    */
   ngOnInit(): void {
     this.authService.clearRedirectUrl();
+    this.configService.findByPropertyName('lr.help.mail').subscribe(remoteData => {
+      this.emailToContact.next(remoteData.payload.values[0]);
+    });
   }
 
 }
