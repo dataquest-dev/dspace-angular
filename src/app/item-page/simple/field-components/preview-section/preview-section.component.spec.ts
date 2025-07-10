a71185e8de5931cc0324c6c5cd26c0e1dbda720f
Juraj Roka <95219754+jr-rk@users.noreply.github.com>
2025-07-10 11:21:14 +0200

    Revert "Fixed spec test to array - not behavioursubject anymore"
    This reverts commit f1df9c29d5561d8e8673a03f668bb81c6304545b.

--- src/app/item-page/simple/field-components/preview-section/preview-section.component.spec.ts
+++ src/app/item-page/simple/field-components/preview-section/preview-section.component.spec.ts
@@ -79,7 +79,10 @@ describe('PreviewSectionComponent', () => {
     expect(mockRegistryService.getMetadataBitstream).toHaveBeenCalled();
   });
 
-  it('should set listOfFiles on init', () => {
-    expect(component.listOfFiles).toEqual([]);
+  it('should set listOfFiles on init', (done) => {
+    component.listOfFiles.subscribe((files) => {
+      expect(files).toEqual([]);
+      done();
+    });
   });
 });
