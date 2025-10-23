32df291b238233a98f10b70e466fc96acabdb95b
Juraj Roka <95219754+jr-rk@users.noreply.github.com>
2025-10-23 15:28:56 +0200

    Adjusted Unit tests for changes in login and logout page components

--- src/app/login-page/login-page.component.spec.ts
+++ src/app/login-page/login-page.component.spec.ts
@@ -12,6 +12,7 @@ import { of } from 'rxjs';
 
 import { APP_DATA_SERVICES_MAP } from '../../config/app-config.interface';
 import { AuthService } from '../core/auth/auth.service';
+import { LocaleService } from '../core/locale/locale.service';
 import { XSRFService } from '../core/xsrf/xsrf.service';
 import { AuthServiceMock } from '../shared/mocks/auth.service.mock';
 import { ActivatedRouteStub } from '../shared/testing/active-router.stub';
@@ -42,6 +43,7 @@ describe('LoginPageComponent', () => {
         { provide: AuthService, useValue: new AuthServiceMock() },
         { provide: XSRFService, useValue: {} },
         { provide: APP_DATA_SERVICES_MAP, useValue: {} },
+        { provide: LocaleService, useValue: { getCurrentLanguageCode: () => 'en' } },
         provideMockStore({}),
       ],
       schemas: [NO_ERRORS_SCHEMA],
