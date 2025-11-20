cb32f8d03ca0cf8cf905b1849d79e171d452b335
Juraj Roka <95219754+jr-rk@users.noreply.github.com>
2025-11-20 17:01:35 +0100

    Lint fix 2

--- src/app/item-page/simple/field-components/specific-field/uri/item-page-uri-field.component.ts
+++ src/app/item-page/simple/field-components/specific-field/uri/item-page-uri-field.component.ts
@@ -1,4 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit
+} from '@angular/core';
 
 import { Item } from '../../../../../core/shared/item.model';
 import { ItemPageFieldComponent } from '../item-page-field.component';
