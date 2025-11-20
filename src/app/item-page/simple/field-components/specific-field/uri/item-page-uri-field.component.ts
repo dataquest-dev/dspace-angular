996c0e60769f96798428b77a993996c9f3d1657e
Juraj Roka <95219754+jr-rk@users.noreply.github.com>
2025-11-20 16:30:40 +0100

    Add DOI resolver configuration and update URI field component to display DOI links properly

--- src/app/item-page/simple/field-components/specific-field/uri/item-page-uri-field.component.ts
+++ src/app/item-page/simple/field-components/specific-field/uri/item-page-uri-field.component.ts
@@ -1,16 +1,22 @@
 import {
   Component,
   Input,
+  OnInit,
 } from '@angular/core';
 
 import { Item } from '../../../../../core/shared/item.model';
+import { MetadataValue } from '../../../../../core/shared/metadata.models';
 import { MetadataUriValuesComponent } from '../../../../field-components/metadata-uri-values/metadata-uri-values.component';
 import { ItemPageFieldComponent } from '../item-page-field.component';
+import _ from 'lodash';
+import { ConfigurationDataService } from '../../../../../core/data/configuration-data.service';
+import { BrowseDefinitionDataService } from '../../../../../core/browse/browse-definition-data.service';
+import { BrowseService } from '../../../../../core/browse/browse.service';
 
 @Component({
   selector: 'ds-item-page-uri-field',
   templateUrl: './item-page-uri-field.component.html',
-  imports: [
+    imports: [
     MetadataUriValuesComponent,
   ],
   standalone: true,
@@ -19,7 +25,14 @@ import { ItemPageFieldComponent } from '../item-page-field.component';
  * This component can be used to represent any uri on a simple item page.
  * It expects 4 parameters: The item, a separator, the metadata keys and an i18n key
  */
-export class ItemPageUriFieldComponent extends ItemPageFieldComponent {
+export class ItemPageUriFieldComponent extends ItemPageFieldComponent implements OnInit {
+
+  doiResolver: string;
+  constructor(protected browseDefinitionDataService: BrowseDefinitionDataService,
+              protected browseService: BrowseService,
+              private configService: ConfigurationDataService) {
+    super(browseDefinitionDataService, browseService);
+  }
 
   /**
    * The item to display metadata for
@@ -41,5 +54,31 @@ export class ItemPageUriFieldComponent extends ItemPageFieldComponent {
    * Label i18n key for the rendered metadata
    */
   @Input() label: string;
+
+  ngOnInit(): void {
+    this.loadDoiResolver();
+  }
+
+  loadDoiResolver() {
+    this.configService.findByPropertyName('identifier.doi.resolver').subscribe(remoteData => {
+      this.doiResolver = remoteData?.payload?.values?.[0];
+    });
+  }
+  getUriMetadataValues() {
+    const mvalues: MetadataValue[] = this.item?.allMetadata(this.fields);
+
+    // Clone the metadata values because it is readonly however I used `let` instead of `const`.
+    let clonedMValues = _.cloneDeep(mvalues);
+    // Compose DOI URI
+    clonedMValues.forEach(mv => {
+      // Filter Metadata values which doesn't have full link. DOI doesn't have full links.
+      if (mv.value.includes('http')) {
+        return;
+      }
+      // Compose `doiResolver` + `doi identifier` value
+      mv.value = this.doiResolver + '/' + mv.value;
+    });
+    return clonedMValues;
+  }
 
 }
