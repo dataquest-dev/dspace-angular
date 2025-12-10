483c965b2a862d10fb5b533cbcc3e443f75ee1ae
Juraj Roka <95219754+jr-rk@users.noreply.github.com>
2025-12-10 14:05:46 +0100

    Header navbar menu update: logo type change & enlarge font size of menu options

--- src/themes/custom/app/header/header.component.ts
+++ src/themes/custom/app/header/header.component.ts
@@ -8,13 +8,16 @@ import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateModule } from '@ngx-translate/core';
 import { Observable } from 'rxjs';
 
+import { LocaleService } from '../../../../app/core/locale/locale.service';
 import { ContextHelpToggleComponent } from '../../../../app/header/context-help-toggle/context-help-toggle.component';
 import { HeaderComponent as BaseComponent } from '../../../../app/header/header.component';
 import { ThemedNavbarComponent } from '../../../../app/navbar/themed-navbar.component';
 import { ThemedSearchNavbarComponent } from '../../../../app/search-navbar/themed-search-navbar.component';
 import { ThemedAuthNavMenuComponent } from '../../../../app/shared/auth-nav-menu/themed-auth-nav-menu.component';
+import { HostWindowService } from '../../../../app/shared/host-window.service';
 import { ImpersonateNavbarComponent } from '../../../../app/shared/impersonate-navbar/impersonate-navbar.component';
 import { ThemedLangSwitchComponent } from '../../../../app/shared/lang-switch/themed-lang-switch.component';
+import { MenuService } from '../../../../app/shared/menu/menu.service';
 
 @Component({
   selector: 'ds-themed-header',
@@ -36,9 +39,25 @@ import { ThemedLangSwitchComponent } from '../../../../app/shared/lang-switch/th
 })
 export class HeaderComponent extends BaseComponent implements OnInit {
   public isNavBarCollapsed$: Observable<boolean>;
+  logoSrc: string;
+
+  constructor(
+    protected menuService: MenuService,
+    protected windowService: HostWindowService,
+    protected localeService: LocaleService,
+  ) {
+    super(menuService, windowService);
+  }
 
   ngOnInit() {
     super.ngOnInit();
     this.isNavBarCollapsed$ = this.menuService.isMenuCollapsed(this.menuID);
+    this.setLogo();
+  }
+
+  setLogo() {
+    this.logoSrc = this.localeService.getCurrentLanguageCode() === 'cs'
+      ? 'assets/images/mendel-uni-logo-w-cs.svg'
+      : 'assets/images/mendel-uni-logo-w-en.svg';
   }
 }
