f7c5e8053087cbf414c63f889b6cce2faa219eea
Juraj Roka <95219754+jr-rk@users.noreply.github.com>
2025-12-18 16:44:55 +0100

    Changed hardcoded email value to email saved in config on BE (mail.helpdesk)

--- src/app/home-page/home-news/home-news.component.ts
+++ src/app/home-page/home-news/home-news.component.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import {
+  Component,
+  OnInit,
+} from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { BehaviorSubject } from 'rxjs';
+
+import { ConfigurationDataService } from '../../core/data/configuration-data.service';
 
 @Component({
   selector: 'ds-base-home-news',
@@ -7,6 +14,7 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './home-news.component.html',
   standalone: true,
   imports: [
+    AsyncPipe,
     TranslateModule,
   ],
 })
@@ -14,5 +22,17 @@ import { TranslateModule } from '@ngx-translate/core';
 /**
  * Component to render the news section on the home page
  */
-export class HomeNewsComponent {
+export class HomeNewsComponent implements OnInit {
+  emailToContact$: BehaviorSubject<string> = new BehaviorSubject<string>('');
+
+  constructor(
+      private configService: ConfigurationDataService,
+  ) {}
+
+  ngOnInit(): void {
+    this.configService.findByPropertyName('mail.helpdesk').subscribe(remoteData => {
+      this.emailToContact$.next(remoteData?.payload?.values?.[0] || '');
+    });
+  }
 }
+
