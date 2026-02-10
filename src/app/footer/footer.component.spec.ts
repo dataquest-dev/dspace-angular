afac0dd1ee238472b9c494d9a50807283a8e3887
Kasinhou <129340513+Kasinhou@users.noreply.github.com>
2026-02-10 15:16:49 +0100

    MENDELU/Fixing e2e tests (#1162)
    * Updated tests to czech
    
    * Add getEndpoint
    
    * Modified failing unit tests
    
    * Fixed comments, sorted, delete unused,...
    
    * Fixed comments, sorted, delete unused,...
    
    * Fixed comments, sorted, delete unused,...
    
    * Divided import
    
    * Fixed login name in cypress test
    
    * Modified homepage cypress test
    
    * Test entire page for accessibility issues
    
    * Removed unnecessary commits
    
    ---------
    
    Co-authored-by: Matus Kasak <matus.kasak@dataquest.sk>

--- src/app/footer/footer.component.spec.ts
+++ src/app/footer/footer.component.spec.ts
@@ -6,10 +6,14 @@ import {
 } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
+import { provideMockStore } from '@ngrx/store/testing';
 import { TranslateModule } from '@ngx-translate/core';
 import { of } from 'rxjs';
 
-import { APP_CONFIG } from '../../config/app-config.interface';
+import {
+  APP_CONFIG,
+  APP_DATA_SERVICES_MAP,
+} from '../../config/app-config.interface';
 import { environment } from '../../environments/environment.test';
 import { NotifyInfoService } from '../core/coar-notify/notify-info/notify-info.service';
 import { ConfigurationDataService } from '../core/data/configuration-data.service';
@@ -45,6 +49,8 @@ describe('Footer component', () => {
         { provide: ConfigurationDataService, useValue: mockConfigurationDataService },
         { provide: ActivatedRoute, useValue: new ActivatedRouteStub() },
         { provide: APP_CONFIG, useValue: environment },
+        { provide: APP_DATA_SERVICES_MAP, useValue: {} },
+        provideMockStore(),
       ],
     });
   }));
