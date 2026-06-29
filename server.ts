02d205da80a99a89c7e81ce996b0f8537cbe5078
Matus Kasak <matus.kasak@dataquest.sk>
2026-06-29 14:51:30 +0200

    Keeping higher versions

--- server.ts
+++ server.ts
@@ -248,10 +248,9 @@ function serverSideRender(req, res, next, sendToUser: boolean = true) {
   const { protocol, originalUrl, baseUrl, headers } = req;
   // "allowedHosts" specifies which hosts are allowed to be rendered via SSR.
   // By default, this is set to the host of the UI's baseUrl.
-  const commonEngine = new CommonEngine({
-    enablePerformanceProfiler: environment.ssr.enablePerformanceProfiler,
-    allowedHosts: [ new URL(environment.ui.baseUrl).hostname, 'localhost', '127.0.0.1' ],
-  });
+  const commonEngine = new CommonEngine({ enablePerformanceProfiler: environment.ssr.enablePerformanceProfiler,
+                                          allowedHosts: [ new URL(environment.ui.baseUrl).hostname ],
+                                        });
   // Render the page via SSR (server side rendering)
   commonEngine
     .render({
